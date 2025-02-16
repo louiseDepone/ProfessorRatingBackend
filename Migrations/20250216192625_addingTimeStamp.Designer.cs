aa9dcf3be3ffabd1159b2ed7b09f98b877930a10
louiseDepone <118156909+louiseDepone@users.noreply.github.com>
2025-02-17 03:28:01 +0800

    Adding migration; adding timestamp of creation each  tables

--- Migrations/20250216192625_addingTimeStamp.Designer.cs
+++ Migrations/20250216192625_addingTimeStamp.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ProfessorRating.DbContext;
@@ -11,9 +12,11 @@ using ProfessorRating.DbContext;
 namespace ProfessorRating.Migrations
 {
     [DbContext(typeof(ProfessorRatingDbContext))]
-    partial class ProfessorRatingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250216192625_addingTimeStamp")]
+    partial class addingTimeStamp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250216192625_addingTimeStamp.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProfessorRating.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingTimeStamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "User",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Teacher",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "SchoolDepartment",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "School",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Role",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Rate",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "PinnedRate",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Faculty",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Department",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "CourseLecturer",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Course",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Teacher");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "SchoolDepartment");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "School");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Rate");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "PinnedRate");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Faculty");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "CourseLecturer");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Course");
+        }
+    }
+}
